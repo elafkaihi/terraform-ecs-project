3003c0c32db868c03c84f4ca0c1e071c563f9904
ElMehdiiiii <elmehdi.lafkaihi@centrale-casablanca.ma>
2024-06-04 15:19:30 +0100

    new files

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# env variables
+
+variable "region" {
+  description   = "region to create ressources"
+  type          = string
+}
+
+variable "project_name" {
+  description   = "project name"
+  type          = string
+}
+
+variable "environment" {
+  description   = "environment"
+  type          = string
+}
